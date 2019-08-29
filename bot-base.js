146f3f24615a1813cc8a33ca34bd64da3634d21e
yandere chan <51772725+yc8690347@users.noreply.github.com>
2019-08-28 23:23:20 -0400

    Update bot-base.js

--- bot-base.js
+++ bot-base.js
@@ -1,12 +1,11 @@
 var WebSocketClient = require("websocket").client;
 var chalk = require("chalk");
 
-//Config
 class Config {
 	constructor() {
 		this.conxTimeout = 600;
-		this.ip = "170.75.163.216:6004";
-		this.name = guest(); 
+		this.ip = "104.219.234.138:6004";
+		this.name = 'YandereBot';
 	}
 }
 var config = new Config();
@@ -14,57 +13,56 @@ var config = new Config();
 function connectToServer() {
 	var ws = new WebSocketClient();
 
-	console.log(chalk.blue("Connecting..."));
+	console.log(chalk.blue('Connecting to VM1...'));
 
 	ws.on("connectFailed", function(error) {
-		console.log(chalk.red("Connect Error: " + error.toString()));
+		console.log(chalk.red('Failed to connect: ' + error.toString()));
 		connectToServer();
 	});
 
 	ws.on("connect", function(conx) {
 
-		console.log(chalk.green("WebSocket Client Connected!"));
+		console.log(chalk.green('Connected to VM1 as YandereBot!'));
 
-		function voteYes() { //*
+		function voteYes() {
 			conx.sendUTF(encodeCommand(['vote', '1']));
 		}
 
-		function voteNo() { //*
+		function voteNo() {
 			conx.sendUTF(encodeCommand(['vote', '0']));
 		}
 
 		function chat(message) {
 			conx.sendUTF(encodeCommand(['chat', message]));
 		}
-		// Get key ID on your own by looking at websocket traffic in chrome(Dev Tools > Network > Click WS > Click 170.whatever > Refresh > take turn > type some letters, the 65xxx in the data with key in it is the key's ID)
-		function sendKey(keyID) { //*
+		
+		function sendKey(keyID) {
 			conx.sendUTF(encodeCommand(['key', keyID, '1']));
 			conx.sendUTF(encodeCommand(['key', keyID, '0']));
 		}
 
-		function getTurn() { //*
+		function getTurn() {
 			conx.sendUTF(encodeCommand(['turn', '1']));
 		}
-		// Commands marked by a * require that you connect to a vm before using. VM names are in the url, and if they are not, look at the websocket traffic for the 'connect' message sent when clicking a VM on /collab-vm/
+		 
 		function connect(name) {
 			conx.sendUTF(encodeCommand(['connect', name]));
 		}
 		conx.on("error", function(error) {
-			console.log(chalk.red.bgYellow.bold("Connection Error: " + error.toString()));
+			console.log(chalk.red.bgYellow.bold('Error while connecting: ' + error.toString()));
 			setTimeout(function() {
 				connectToServer();
 			}, config.conxTimeouut);
 		});
 
 		conx.on("close", function() {
-			console.log(chalk.red.bgYellow.bold("Connection Closed!"));
+			console.log(chalk.red.bgYellow.bold('Connection was closed.'));
 			setTimeout(function() {
 				connectToServer();
 			}, config.conxTimeout);
 		});
 
 		conx.on("message", function(message) {
-			// If you want to make the bot do things whenever someone sends a message, put stuff here. The parameter that gets passed to the callback function contains the message someone sends.
 		});
 
 		var user = config.name;
@@ -76,28 +74,13 @@ function connectToServer() {
 			}
 		}, 2500);
 		if (conx.connected) {
-			//connect('win7');
-			//If you want the bot to do anything upon connection, put it all here
+			chat('YandereBot v1.1 by Yandere Chan.');
 		}
 
 	});
 	ws.connect("ws://" + config.ip, "guacamole");
 }
 connectToServer();
-
-function guest() {
-	function num() {
-		var text = "";
-		var pool = "0123456789";
-
-		for (var i = 0; i < 5; i++)
-			text += pool.charAt(Math.floor(Math.random() * pool.length));
-
-		return text;
-	}
-	var a = "guest" + num();
-	return a;
-}
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
